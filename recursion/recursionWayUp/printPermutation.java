dc702d81bfe4f310d940e3d3e2abb11d7cbd6ff7
princekumar21 <pk41330@gmail.com>
2021-05-14 04:03:56 +0530

    updated

--- recursion/recursionWayUp/printPermutation.java
+++ recursion/recursionWayUp/printPermutation.java
@@ -1,5 +1,7 @@
 import java.util.*;
 public class printPermutation {
+
+    //permutation without repetition
     public static void printpermutation(String str, String ans){
 
         if(str.length() == 0){
@@ -17,11 +19,66 @@ public class printPermutation {
 
 
     }
+
+    public static int countprintpermutation(String str, String ans){
+
+        if(str.length() == 0){
+            System.out.println(ans);
+            return 1;
+        }
+        int count = 0;
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            String roq = str.substring(0, i) + str.substring(i + 1);
+
+            count += countprintpermutation(roq, ans + ch);
+
+        }
+
+        return count;
+        
+
+
+    }
+    //permutation with repetation
+    public static int countprintpermutationNoRepetition(String str, String ans){
+
+        if(str.length() == 0){
+            System.out.println(ans);
+            return 1;
+        }
+        int count = 0;
+
+        char cha = '$';
+        for(int i = 0; i < str.length(); i++){
+
+            
+            
+            if(str.charAt(i) != cha){
+
+                char ch = str.charAt(i);
+                String roq = str.substring(0, i) + str.substring(i + 1);
+                count += countprintpermutationNoRepetition(roq, ans + ch);
+
+
+            }
+
+            cha = str.charAt(i);
+            
+
+        }
+
+        return count;
+        
+
+
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         String ans = "";
-        printpermutation(str, ans);
+        //printpermutation(str, ans);
+        System.out.println(countprintpermutationNoRepetition(str, ans));
 
 
     }
