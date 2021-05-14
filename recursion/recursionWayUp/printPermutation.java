9a5c6831e1a045bc50a740c2dad489fe9dee07aa
princekumar21 <pk41330@gmail.com>
2021-05-15 03:34:51 +0530

    updated

--- recursion/recursionWayUp/printPermutation.java
+++ recursion/recursionWayUp/printPermutation.java
@@ -40,10 +40,50 @@ public class printPermutation {
 
 
     }
-    //permutation with repetition
-    public static int countprintpermutationNoRepetition(String str, String ans){
-        //first we will sort the string
+    
+    //permutation without repetition
+    public static void permutationNoRepetition(String str, String ans){
+        if(str.length() == 0){
+            System.out.println(ans);
+        }
+        
+        char prev = '$';
+        for(int i = 0; i < str.length(); i++){
 
+            if(str.charAt(i) != prev ){
+                char ch = str.charAt(i);
+                String roq = str.substring(0, i) +  str.substring(i + 1);
+                permutationNoRepetition(roq, ans + ch);
+
+            }
+
+            prev = str.charAt(i);
+            
+        }
+        
+    }
+
+    //permutation with repetition
+    public static void permutationNoRepetition(String str){
+        //first we will sort the string by storing it in freq array and then we will change it to string
+
+        StringBuilder sb = new StringBuilder();
+
+        int freq[] = new int[26];
+        for(int i = 0; i < str.length(); i++){
+           int alphabetIndex = str.charAt(i) - 'a';
+            freq[alphabetIndex]++;
+        }
+        
+        //appended the char to sb from freq array
+        for(int i = 0; i < freq.length; i++){
+            for(int j = 0; j < freq[i]; j++){
+                sb.append((char)(i + 'a'));              
+            }
+        }
+        
+        
+        permutationNoRepetition(sb.toString(), "");
         
         
 
@@ -52,9 +92,9 @@ public class printPermutation {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        String ans = "";
+        //String ans = "";
         //printpermutation(str, ans);
-        System.out.println(countprintpermutationNoRepetition(str, ans));
+        permutationNoRepetition(str);
 
 
     }
