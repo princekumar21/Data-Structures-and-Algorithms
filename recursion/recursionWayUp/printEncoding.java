7f8abe2e8268ae38f02b79ee23dbf8a6d0b8aef5
princekumar21 <pk41330@gmail.com>
2021-05-16 04:26:40 +0530

    update

--- recursion/recursionWayUp/printEncoding.java
+++ recursion/recursionWayUp/printEncoding.java
@@ -1,3 +1,13 @@
 public class printEncoding {
     
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        //String ans = "";
+        printpermutation(str, "");
+        //permutationNoRepetition(str);
+
+
+    }
+    
 }
