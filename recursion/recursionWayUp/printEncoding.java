e0bf94847c77603b21a5451db85ec0ab7df701dc
princekumar21 <pk41330@gmail.com>
2021-05-17 00:22:17 +0530

    update

--- recursion/recursionWayUp/printEncoding.java
+++ recursion/recursionWayUp/printEncoding.java
@@ -1,10 +1,34 @@
+import java.util.*;
 public class printEncoding {
+
+    public static void printEncodings(String str, String ans) {
+        if(str.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+        
+        char ch = str.charAt(0);
+        if(ch == '0'){
+            return;
+        }
+        
+        printEncodings(str.substring(1), ans + (char)('a' + (ch - '0') - 1));
+        
+        if(str.length() > 1){
+            int num = (ch - '0') * 10 + str.charAt(1) - '0';
+            
+            if(num <= 26){
+                printEncodings(str.substring(2), ans + (char)((num - 1) + 'a'));
+            }
+        }
+        
+    }
     
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         //String ans = "";
-        printpermutation(str, "");
+        printEncodings(str, "");
         //permutationNoRepetition(str);
 
 
