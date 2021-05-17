f50c3e83b0cafed3ccca75041a937b7c922e062f
princekumar21 <pk41330@gmail.com>
2021-05-18 02:09:52 +0530

    update

--- timeAndSpace/sort01.java
+++ timeAndSpace/sort01.java
@@ -1,17 +1,30 @@
 import java.util.Scanner;
 public class sort01{
-    public static void swap(int[] arr, int itr, int p1){
-        int temp = arr[itr];
-        arr[itr] = arr[p1];
-        arr[p1] = temp;
+    public static int[] sort012(int[] arr){
+        int n = arr.length,  p = -1, itr = 0, p2 = n - 1;
+        
+        while(itr <= p2){
 
+            if(arr[itr] == 0){
+                swap(arr, itr++, ++p);
+            }else if(arr[itr] == 2){
+
+                swap(arr, itr, p2--);
+              
+            }else{
+                itr++;
+            }
+
+        }
+        return arr; 
     }
+    
     public static int[] sort011(int[] arr){
         int p = -1;
         int itr = 0;
 
         while(itr < arr.length){
-            
+
             if(arr[itr] == 0){
                 swap(arr, itr, ++p);
             }
@@ -22,6 +35,13 @@ public class sort01{
         return arr;
 
     }
+
+    public static void swap(int[] arr, int itr, int p1){
+        int temp = arr[itr];
+        arr[itr] = arr[p1];
+        arr[p1] = temp;
+
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -29,7 +49,8 @@ public class sort01{
         for(int i = 0; i < arr.length; i++){
             arr[i] = sc.nextInt();
         }
-        int[] arr1 = sort011(arr);
+        //int[] arr1 = sort011(arr);
+        int[] arr1 = sort012(arr);
         for(int a : arr1){
             System.out.print(a + " ");
         }
