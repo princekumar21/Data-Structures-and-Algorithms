f1f57497f928c145a74623582e126e8adb1951cb
princekumar21 <pk41330@gmail.com>
2021-05-18 04:00:37 +0530

    update

--- timeAndSpace/mergeSortedArray.java
+++ timeAndSpace/mergeSortedArray.java
@@ -1,31 +1,56 @@
 import java.util.*;
 public class mergeSortedArray {
-    public static int[] mergesortedArray(int[] arr, int[] arr1){
-        int n = arr.length, m = arr1.length;
+    public static int[] mergesortedArray(int[] arr1, int[] arr2){
+        int n = arr1.length, m = arr2.length;
         int ans[] = new int[m + n];
         int i = 0, j = 0, k = 0;
 
         while(i < n && j < m){
-            if(arr[i] < arr1[j]){
-                ans[k++] = arr[i++];
-            }else if(arr[i] >= arr1[j]){
-                ans[k++] = arr1[j++];
+            if(arr1[i] < arr2[j]){
+                ans[k++] = arr1[i++];
+            }else{
+                ans[k++] = arr2[j++];
 
             }
         }
 
         while(i < n){
            
-                ans[k++] = arr[i++];
+                ans[k++] = arr1[i++];
 
         }
 
         while(j < m){
            
-                ans[k++] = arr1[j++];
-           
+                ans[k++] = arr2[j++];
+           }
+            return ans;
 
+    }
+
+    //above code is better in terms of time complexity
+    public static int[] mergesortedArray2(int[] arr1, int[] arr2){
+        int n = arr1.length, m = arr2.length;
+        int ans[] = new int[m + n];
+        int i = 0, j = 0, k = 0;
+
+        while(i < n || j < m){
+            if(i == n){
+                ans[k++] = arr2[j++];
+            }
+            else if(j == m){
+                ans[k++] = arr1[i++];
+            }
+            if(arr1[i] < arr2[j]){
+
+                ans[k++] = arr1[i++];
+
+            }else{
+                ans[k++] = arr2[j++];
+
+            }
         }
+
             return ans;
 
     }
@@ -40,12 +65,13 @@ public class mergeSortedArray {
         }
 
         int n1 = sc.nextInt();
+
         int arr2[] = new int[n1];
         for(int i = 0; i < arr2.length; i++){
             arr2[i] = sc.nextInt();
         }
-        //int[] arr1 = sort011(arr);
-        int[] arr3 = mergesortedArray(arr1, arr2);
+        
+        int[] arr3 = mergesortedArray2(arr1, arr2);
         for(int a : arr3){
             System.out.print(a + " ");
         }
