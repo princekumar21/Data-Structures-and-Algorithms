12c602db3e3839fb6e826160d0d66158b6af33b7
princekumar21 <pk41330@gmail.com>
2021-05-26 03:20:03 +0530

    update

--- linkedList/linkedlist.java
+++ linkedList/linkedlist.java
@@ -21,33 +21,42 @@ public class linkedlist{
 
         if(this.size == 0){
             this.tail = this.head = node;
-            
         }else {
             node.next = this.head;
             this.head = node;
-
         }
         this.size++;
-
     }
 
     public void addFirst(int data){
         Node node = new Node();
         node.data = data;
-        
         addFirstNode(node);
     }
 
     public void addLastNode(Node node){
-        
-
+        if(this.size == 0){
+            this.head = this.tail = node;
+        }else{
+            tail.next = node;
+            this.tail = node;
+        }
+        this.size++;
     }
-
+    
     public void addLast(int data){
         Node node = new Node();
         node.data = data;
 
         addLastNode(node);
+    }
+
+    private Node removeFirstNode(){
+
+    }
+
+    public Node removeFirst(){
+        
     }
 
     public static void main(String args[]){
