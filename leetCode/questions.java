e0fe4bf98213daea9b151f3757b20c5c5c2fe07a
princekumar21 <pk41330@gmail.com>
2021-06-05 23:49:41 +0530

    update

--- leetCode/questions.java
+++ leetCode/questions.java
@@ -18,30 +18,6 @@ public class questions {
         }
     
     //19
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head == null || head.next == null) return null;
-        
-        ListNode slow = head;
-        ListNode fast = head;
-        while(n-- > 0){
-            fast = fast.next;
-        }
-        
-        if(fast == null){
-            return head.next;
-            
-        }
-        
-        while(fast.next != null){
-            fast = fast.next;
-            slow = slow.next;
-        }
-      
-        slow.next = slow.next.next;
-        
-        
-        return head;
-        
-    }
+    
     
 }
