411d243b01ec0a897855332e8c740eb881e65366
princekumar21 <pk41330@gmail.com>
2021-06-08 02:17:49 +0530

    update

---
+++ stack/stack.java
@@ -0,0 +1,26 @@
+public class stack {
+    private int[] arr;
+    private int tos;
+    private int size;
+    private int maxsize;
+
+    private void initialize(int data){
+        this.arr[data];
+        
+
+    }
+
+    stack(){
+        initialize(5);
+    }
+
+    stack(int data){
+        initialize(data);
+    }
+
+
+    public void push(int data){
+
+
+    }
+}
