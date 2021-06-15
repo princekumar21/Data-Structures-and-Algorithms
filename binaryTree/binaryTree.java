0189ce41c78c0726541c9beaa0f18bf67b80e792
princekumar21 <pk41330@gmail.com>
2021-06-15 19:49:15 +0530

    update

--- binaryTree/binaryTree.java
+++ binaryTree/binaryTree.java
@@ -1,24 +1,24 @@
 import java.util.ArrayList;
 
-public class binaryTree{
+public class binaryTree {
     public static class Node {
         int data;
         Node left;
         Node right;
 
-        Node(int data, Node left, Node right){
+        Node(int data, Node left, Node right) {
             this.data = data;
             this.left = left;
             this.right = right;
         }
 
-        Node(int data){
+        Node(int data) {
             this(data, null, null);
         }
     }
 
-    public static void preOrder(Node root, ArrayList<Integer> ans){
-        if(root == null){
+    public static void preOrder(Node root, ArrayList<Integer> ans) {
+        if (root == null) {
             return;
         }
 
@@ -27,8 +27,9 @@ public class binaryTree{
         preOrder(root.right, ans);
 
     }
-    public static void inOrder(Node root, ArrayList<Integer> ans){
-        if(root == null){
+
+    public static void inOrder(Node root, ArrayList<Integer> ans) {
+        if (root == null) {
             return;
         }
 
@@ -37,8 +38,8 @@ public class binaryTree{
         inOrder(root.right, ans);
     }
 
-    public static void postOrder(Node root, ArrayList<Integer> ans){
-        if(root == null){
+    public static void postOrder(Node root, ArrayList<Integer> ans) {
+        if (root == null) {
             return;
         }
 
@@ -47,11 +48,11 @@ public class binaryTree{
         ans.add(root.data);
     }
 
-    public static int size(Node node){
-        if(node == null){
+    public static int size(Node node) {
+        if (node == null) {
             return 0;
         }
-        
+
         int leftSize = size(node.left);
         int rightSize = size(node.right);
 
@@ -59,71 +60,96 @@ public class binaryTree{
         // return node == null ? 0 : size(node.left) + size(node.right) + 1;
     }
 
-    public static int sum(Node node){
-        if(node == null) return 0;
+    public static int sum(Node node) {
+        if (node == null)
+            return 0;
 
         int leftSum = sum(node.left);
         int rightSum = sum(node.right);
-        
+
         return leftSum + rightSum + node.data;
 
         // return node == null ? 0 : sum(node.left) + sum(node.right) + node.data;
     }
 
-    public static int max(Node node){
-        if(node == null){
+    public static int max(Node node) {
+        if (node == null) {
             return -(int) 1e9;
         }
-        
-       int leftMax = max(node.left);
-       int rightMax = max(node.right);
 
-       return Math.max(node.data, Math.max(leftMax, rightMax));
+        int leftMax = max(node.left);
+        int rightMax = max(node.right);
 
-    //    return node == null ? -(int) 1e9 : Math.max(node.data, Math.max(max(node.left), max(node.right)));
+        return Math.max(node.data, Math.max(leftMax, rightMax));
+
+        // return node == null ? -(int) 1e9 : Math.max(node.data,
+        // Math.max(max(node.left), max(node.right)));
 
     }
 
-    public static int height(Node node){
-        if(node == null){
+    public static int height(Node node) {
+        if (node == null) {
             return -1;
         }
 
-       int leftHeight =  height(node.left);
-       int rightHeight = height(node.right);
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
 
-       return Math.max(leftHeight, rightHeight) + 1;
-        
+        return Math.max(leftHeight, rightHeight) + 1;
+
     }
 
-    public static int countLeaves(Node node){
-        if(node == null){
+    public static int countLeaves(Node node) {
+        if (node == null) {
             return 0;
         }
 
-        if(node.right == null && node.left == null){
+        if (node.right == null && node.left == null) {
             return 1;
         }
 
-        
-      return countLeaves(node.left) + countLeaves(node.right);
+        return countLeaves(node.left) + countLeaves(node.right);
     }
 
-    public static void exactlyOneChild(Node node, ArrayList<Integer> ans){
-        if(node == null || (node.left == null && node.right == null)){
+    public static void exactlyOneChild(Node node, ArrayList<Integer> ans) {
+        if (node == null || (node.left == null && node.right == null)) {
             return;
         }
 
-        if(node.left == null || node.right == null){
-                ans.add(node.data);
+        if (node.left == null || node.right == null) {
+            ans.add(node.data);
         }
 
         exactlyOneChild(node.left, ans);
         exactlyOneChild(node.right, ans);
 
+    }
+
+    public static boolean findData(Node node, int data) {
+        if (node == null) {
+            return false;
+        }
+
+        if (node.data == data) {
+            return true;
+        }
+
+        return findData(node.left, data) || findData(node.right, data);
+    }
+
+    public static boolean nodeToRootPath (Node node, int data, ArrayList<Integer> ans){
+        if(node == null) {
+            return 
+        }
+
+        
+
+
 
     }
 
-    
+    public static void printKlevelsDown(Node root, int k, ArrayLis<Integer> ans) {
+
+    }
 
 }
