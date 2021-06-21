cfb95c533e65a9024a4ea646d1fb0422c204a933
princekumar21 <pk41330@gmail.com>
2021-06-22 01:58:53 +0530

    update

--- binaryTree/BST/BST.java
+++ binaryTree/BST/BST.java
@@ -25,5 +25,11 @@ public class BST {
         return size(node.left) + size(node.right) + 1;
     }
 
-    
+    public static int height(Node node){
+        if(node == null){
+            return -1;
+        }
+
+        return Math.max(height(node.left), height(node.right)) + 1;
+    }
 }
