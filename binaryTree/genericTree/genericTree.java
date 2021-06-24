d51e3a7c475cc5153656633dbf8db8e273b58d30
princekumar21 <pk41330@gmail.com>
2021-06-25 00:09:35 +0530

    update

--- binaryTree/genericTree/genericTree.java
+++ binaryTree/genericTree/genericTree.java
@@ -2,13 +2,20 @@ import java.util.ArrayList;
 public class genericTree {
     public static class Node {
         int data;
-        ArrayList<Node> node ;
+        ArrayList<Node> child ;
 
         Node(int data){
             this.data = data;
-            this.node = new ArrayList<>();
+            this.child = new ArrayList<>();
         }
     }
-
+    
+    public static int size(Node node){
+        int count = 0;
+        for(Node children : node.child){
+            count += size(children);
+        }
+        return count + 1;
+      }
     
 }
