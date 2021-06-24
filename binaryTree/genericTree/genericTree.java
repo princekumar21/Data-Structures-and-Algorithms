914572e4ada692ec155a456d1e8f94becb32d78d
princekumar21 <pk41330@gmail.com>
2021-06-25 01:58:37 +0530

    update

--- binaryTree/genericTree/genericTree.java
+++ binaryTree/genericTree/genericTree.java
@@ -16,6 +16,44 @@ public class genericTree {
             count += size(children);
         }
         return count + 1;
-      }
+    }
+
+    public static boolean nodeToRootPath(Node node, int data, ArrayList<Integer> list){
+        if(node.data == data){
+            list.add(node.data);
+            return true;
+        }
+
+        boolean found = false;
+        for(Node children : node.child){
+            found = nodeToRootPath(children, data, list) || found;
+        }
+
+        if(found){
+            list.add(node.data);
+        }
+
+        return found;
+
+
+    }
+    public static ArrayList<Integer> nodeToRootPath(Node node, int data){
+        if(node.data == data){
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(node.data);
+            return list;
+        } 
+        ArrayList<Integer> path = new ArrayList<>();
+        for(Node child : node.children){
+           path = nodeToRootPath(child, data);
+           if(path.size() != 0){
+                break;
+            }
+        }
+         if(path.size() != 0){
+                path.add(node.data);
+            }
+        return path;
+ }
     
 }
