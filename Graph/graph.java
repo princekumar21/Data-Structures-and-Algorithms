8ff1e433ffab57366e035846439b36759499a311
princekumar21 <pk41330@gmail.com>
2021-07-10 23:46:58 +0530

    update

--- Graph/graph.java
+++ Graph/graph.java
@@ -85,7 +85,16 @@ public class graph {
         return count;
     }
 
-    
+    public static void preOrder(ArrayList<Edge>[] graph, int src, boolean[] vis, int wsf, String psf) {
+        System.out.println(src + " -> " + (psf + src) + " @ " + wsf);
+        vis[src] = true;
+        for (Edge e : graph[src]) {
+            if (!vis[e.nbr])
+                preOrder(graph, e.nbr, vis, wsf + e.wt, psf + src);
+        }
+
+        vis[src] = false;
+    }
 
     public static void postOrder(ArrayList<Edge>[] graph, int src, boolean[] vis, int wsf, String psf) {
         vis[src] = true;
