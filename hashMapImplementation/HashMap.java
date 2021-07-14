dba2066da6c315350ea64290c6b9b2cf2709f87a
princekumar21 <pk41330@gmail.com>
2021-07-14 20:43:35 +0530

    update

---
+++ hashMapImplementation/HashMap.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+public class HashMap {
+    private class Node {
+        Integer key = null;
+        Integer value = null;
+
+        Node(Integer key, Integer value){
+            this.key = key;
+            this.value = value;
+        
+        }
+
+    }
+        private LinkedList<Node>[] Buckets;
+        private int bucketLen = 0;
+        private int TotalNofNode = 0;
+
+
+        private 
+        
+    
+
+
+}
+
+
+
