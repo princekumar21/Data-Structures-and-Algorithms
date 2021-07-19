a99638e786d950b0d544676f3fef4a2600e686c4
princekumar21 <pk41330@gmail.com>
2021-07-19 23:06:21 +0530

    update

--- numberSystem/baseconversion.java
+++ numberSystem/baseconversion.java
@@ -45,7 +45,19 @@ public class baseconversion {
         }
         return ans;
     }
-    
+    public static int anyBaseToDecimal(int n, int b){
+        int ans = 0;
+        int pow = 1;
+        while(n != 0){
+            int rem = n % 10;
+            ans += rem * pow;
+            pow *= b;
+            n /= 10;
+
+        }
+        return ans;
+
+    }
 
 
     
