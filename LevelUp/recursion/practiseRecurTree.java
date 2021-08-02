2aa8b36d2dc0034c77798184df01373819868d96
princekumar21 <pk41330@gmail.com>
2021-08-03 03:43:14 +0530

    update

---
+++ LevelUp/recursion/practiseRecurTree.java
@@ -0,0 +1,44 @@
+public class practiseRecurTree {
+    //this is also a permutation means we acn a make a choice use coins repeatidily
+    public static int InfiCoins(int [] arr, int tar, String asf){
+        if(tar == 0){
+            System.out.println(asf);
+            return 1;
+        }
+        int count = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(tar - arr[i] >= 0){
+                count += InfiCoins(arr, tar - arr[i],  asf  + i + " ");
+
+            }
+        }
+        return count;
+
+    }
+
+    public static int limitCoins(int arr[], int tar, String asf, int idx){
+        if(tar == 0){
+            System.out.println(asf );
+            return 1;
+        }
+        int count = 0;
+        for(int i = idx; i < arr.length; i++){
+            if(tar - arr[i] >= 0 ){
+               
+               count +=  limitCoins(arr, tar - arr[i], asf  + arr[i], i);
+                
+
+            }
+          
+        }
+        return count;
+
+    }
+
+    public static void main(String[] args){
+        int arr[] = {2,3,5,7};
+        boolean vis[] = new boolean[arr.length];
+        // InfiCoins(arr, 10, "");
+        limitCoins(arr, 10, "", 0);
+    }
+}
