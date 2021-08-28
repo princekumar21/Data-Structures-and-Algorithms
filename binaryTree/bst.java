797a89e88aa80e41c5bc1f4471ca7be18d4f14ee
princekumar21 <pk41330@gmail.com>
2021-08-29 04:18:55 +0530

    update

--- binaryTree/bst.java
+++ binaryTree/bst.java
@@ -35,4 +35,37 @@ public class bst {
         return true;
 
     }
+    
+    public static class isBSTPair {
+        boolean isBst = true;
+        long maxEle = -(long) 1e18;
+        long minEle = (long) 1e18;
+    }
+
+    public static isBSTPair isBST_02(Node root) {
+        if (root == null) {
+            return new isBSTPair();
+        }
+
+        isBSTPair left = isBST(root.left);
+        if (!left.isBst) {
+            return left;
+        }
+
+        isBSTPair right = isBST(root.right);
+        if (!right.isBst) {
+            return right;
+        }
+
+        isBSTPair self = new isBSTPair();
+        self.isBst = false;
+        if (left.maxEle < root.data && right.minEle > root.data) {
+            self.maxEle = Math.max(right.maxEle, root.data);
+            self.minEle = Math.min(left.minEle, root.data);
+            self.isBst = true;
+
+        }
+        return self;
+
+    }
 }
