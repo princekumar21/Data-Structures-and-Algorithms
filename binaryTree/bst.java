951010ad2062124f7d7b5bd7c0e9e26b4fd0b51a
princekumar21 <pk41330@gmail.com>
2021-08-29 01:46:41 +0530

    update

--- binaryTree/bst.java
+++ binaryTree/bst.java
@@ -1,23 +1,38 @@
 public class bst {
-    public static class Node{
+    public static class Node {
         int data;
         Node left;
         Node right;
 
-        Node(int data, Node left, Node right){
+        Node(int data, Node left, Node right) {
             this.data = data;
             this.left = left;
             this.right = right;
         }
 
-        Node(int data){
+        Node(int data) {
             this(data, null, null);
         }
 
     }
 
-    Node prev = null;
-    public static boolean isBST(Node root){
+    static Node prev = null;
+
+    public static boolean isBST(Node root) {
+        if (root == null)
+            return true;
+
+        if (!isBST(root.left))
+            return false;
+
+        if (prev != null && prev.data >= root.data)
+            return false;
+        prev = root;
+
+        if (!isBST(root.right))
+            return false;
+
+        return true;
 
     }
 }
